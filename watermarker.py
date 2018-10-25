3ee4d6c7a95a59a214ff7751d86712a2d5c08d5b
Wesley Guergah <wguergah10556@g.ucla.edu>
2018-10-24 18:56:27 -0700

    Made some edits, added a diagnol iteration method for images

--- watermarker.py
+++ watermarker.py
@@ -1,5 +1,6 @@
 from PIL import Image
 import numpy as np
+import math
 
 im = Image.open("owl.jpg")
 print("Welcome to the watermark picture editor")
@@ -7,7 +8,7 @@ arr = np.array(im)
 edited = arr
 print("Our image is {} rows {} columns and has a depth of {}, corresponding to the RGB values of each pixel in the image".format(len(arr),len(arr[0]),len(arr[0][1])))
 
-
+'''
 # Make the image black
 
 for i in range(0,len(edited)):
@@ -58,17 +59,50 @@ for i in range(0,len(edited)):
 dotted = Image.fromarray(edited)
 # dotted.show()
 
+'''
 
-# Diagnol lines
 
-for i in range(0,len(edited)):
-	for j in range(0,len(edited[0])):
-		for k in range(0,127):
-				edited[0,2*k]=[255,255,255]
+# Trippy pattern, Diagnol iteration
+
+m = len(edited) - 1    # Number of rows 
+n = len(edited[0]) - 1 # Numbers of columns 
+
+for k in range(0,m):
+	if (k%2==0):	
+		i=k
+		j=0
+		while (i >=0 ):
+			edited[i,j]=[i,j,i^j]
+			i = i - 1
+			j = j+1
+
+for k in range(0,n):
+	if (k%2==0):
+		i=m
+		j=k
+		while(j < n):
+			edited[i,j]=[i,j,i^j]
+			i=i-1
+			j=j+1
+
+
+for k in range(0,n):
+	if (k%5==0):	
+		i=k
+		j=0
+		while (i >=0 ):
+			edited[i,j]=[i^j,j^i^j,i^j^k]
+			i = i - 1
+			j = j+1
 
 
 black = Image.fromarray(edited)
 black.show()
+
+
+black = Image.fromarray(edited)
+black.show()
+
 
 
 
